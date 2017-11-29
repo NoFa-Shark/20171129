e7e68a3607bfeb129d347fbf9806afd15225bd7d
Shark <shark85423@gmail.com>
2017-11-29 11:36:19 +0800

    建立 testPostCreatePage

---
+++ tests/Feature/PostContollerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostContollerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testPostCreatePage()
+    {
+        $this->assertTrue(true);
+    }
+}
